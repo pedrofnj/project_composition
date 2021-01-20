cc904bb9e6db54ef785b78b608427f3c9d1d5139
Pedro Felix <pedrofelixnunes.j@gmail.com>
2021-01-20 00:07:43 -0300

    project_composition

---
+++ src/application/Program.java
@@ -0,0 +1,64 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
+public class Program {
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Cliente data: ");
+		System.out.print("Name: ");
+		String clienName = sc.nextLine();
+		System.out.print("Email: ");
+		String clientEmail = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
+		
+		Client client = new Client(clienName, clientEmail, birthDate);
+		
+		System.out.println("Enter order data: ");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		Order order = new Order(new Date(), status, client);
+		
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.println("Enter #" + (i + 1) +" item data:");
+			sc.nextLine();
+			System.out.print("Product name: ");
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			double productPrice = sc.nextDouble();
+			
+			Product product = new Product(productName, productPrice);
+			
+			System.out.print("Quantity: ");
+			int productQuantity = sc.nextInt();
+			
+			OrderItem orderItem = new OrderItem(productQuantity, productPrice, product);
+			
+			order.addItem(orderItem);
+			
+		}
+		
+			System.out.println();
+			System.out.println("ORDER SUMMARY:");
+			System.out.println(order);
+		
+		sc.close();
+	}
+
+}
